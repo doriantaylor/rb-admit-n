8f81f3ec134e5f144907dbd3c8bc11d1c4d9b01d
Dorian Taylor <code@doriantaylor.com>
2026-03-09 23:14:44 -0400

    i think we have a skeleton

--- README.md
+++ README.md
@@ -14,18 +14,20 @@ detail](https://senseatlas.net/82a04484-0bf1-48e7-8468-6c01be12e314).
 
 # Design
 
-The main thing this microservice needs to do is deal with the outcome
-of a customer making a purchase. All _that_ consists of is enrolling
-one or more e-mail addresses into an access control database, and in
-the base case, forwarding the customer on to where they can enjoy
-their purchase.
+The main thing [this
+microservice](https://senseatlas.net/19b0ccd6-4602-4aa3-a51f-2149080111c8)
+needs to do is deal with the outcome of a customer making a
+purchase. All _that_ consists of is enrolling one or more e-mail
+addresses into an access control database, and in the base case,
+forwarding the customer on to where they can enjoy their purchase.
 
 The catch, here, is one _or more_. One use case I want to be able to
-handle is a manager purchasing a block of audience slots for their
-team. As such, there needs to be UI for the manager to assign slots to
-e-mail addresses. The manager, furthermore, may not be interested in
-the content for themselves, so I don't want to force them to buy an
-extra seat just to manage the other seats they bought.
+handle is [a manager purchasing a block of audience
+slots](https://senseatlas.net/e061cc87-2a85-45dc-bf31-0062fb12b9b2)
+for their team. As such, there needs to be UI for the manager to
+assign slots to e-mail addresses. The manager, furthermore, may not be
+interested in the content for themselves, so I don't want to force
+them to buy an extra seat just to manage the other seats they bought.
 
 So there needs to be an "account manager" role distinct from the
 "audience" who actually gets access to the content, but only as long
@@ -50,9 +52,17 @@ consists of the one resource where the account manager role can
 allocate empty slots. There also needs to be a webhook target for
 receiving payment confirmation asynchronously from Stripe.
 
+## Changes to Authentication/Authorization
+
 This microservice will also be a client to [Forget
 Passwords](https://github.com/doriantaylor/rb-forget-passwords), and
 it will be necessary to modify the latter to be remotely operated.
+
+* enroll user(s)
+* add them to group(s)
+* get a session cookie for a principal (auth bypass)
+
+these just need to be
 
 ## Installation
 
--- lib/admit-n.rb
+++ lib/admit-n.rb
@@ -1,8 +1,133 @@
 # frozen_string_literal: true
 
 require 'admit-n/version'
+require 'xml/mixup'
+require 'uuid/ncname'
 
+# Admit-_N_ performs order fulfillment for one-off payments for access
+# to gated content, where the customer can buy access for themselves
+# and/or any number of others.
+#
 module AdmitN
-  class Error < StandardError; end
-  # Your code goes here...
+
+  # This part is the actual Web application.
+  #
+  class App
+    include XML::Mixup
+
+    public
+
+    def initialize
+    end
+
+    # Set up the checkout session and redirect.
+    #
+    # @param req [Rack::Request] the request object
+    #
+    # @return [Rack::Response] the response
+    #
+    def forward_to_checkout req
+      # generate a nonce to be associated with the checkout session
+
+      # mint a new checkout session
+
+      # redirect to uri
+    end
+
+    # Receive the redirection from stripe
+    #
+    # @param req [Rack::Request] the request object
+    #
+    # @return [Rack::Response] the response
+    #
+    def checkout_landing req
+      # return 405 unless the method is GET
+      #
+      # get the nonce and the checkout session id
+
+      # only if we have beaten the webhook:
+      # * call out to API to verify payment success
+      #   (return 503 if reaching out to stripe fails)
+      # * enroll customer to auth database
+
+      # obtain session cookie?
+
+      # redirect to assign/confirm
+    end
+
+    # Dummy service resource that redirects a freshly logged-in user
+    # to the "lobby" depending on who they are:
+    #
+    # * if they are part of the audience then they go to the content
+    # * otherwise they are an account manager
+    #
+    # @param req [Rack::Request] the request object
+    #
+    # @return [Rack::Response] the response
+    #
+    def forward_to_content req
+      # return 405 unless request is a GET
+      # return 401 unless there is a `REMOTE_USER`
+
+      # uhh we need a target to redirect to
+    end
+
+    # Display the available/assigned slots. This may be something that
+    # gets transcluded into another resource.
+    #
+    # @param req [Rack::Request] the request object
+    #
+    # @return [Rack::Response] the response
+    #
+    def display_slots req
+      # return 405 unless request is a GET or POST
+      # return 401 unless there is a `REMOTE_USER`
+
+      # return 403 unless user is authenticated as the customer or
+      # assignee who can grant additional slots
+
+      # maybe we should split between POST and GET
+
+      # POST is just gonna be like a vanilla assign your slot kinda thing
+
+      # GET is gonna be the actual table
+      # (do we want a json variant?)
+
+      # add a "buy more" button if the user is also the customer and
+      # there are two or more slots
+    end
+
+    # Receive asynchronous messages (from Stripe) through a Web hook.
+    #
+    # @note Stripe attempts to retry on an exponential back-off
+    #  schedule if they don't get a `2XX` response code.
+    #
+    # @param req [Rack::Request] the request object
+    #
+    # @return [Rack::Response] the response
+    #
+    def handle_webhook req
+      # return 405 if request method is other than POST
+
+      # return 415 if content type is not json
+
+      # return 401 if no stripe signature header
+
+      # return 403 if stripe signature is bad
+
+      # return 409 if payload is invalid
+
+      # return 202 if we don't handle this payload
+
+      # on `checkout.session.success`:
+      # only if we have beaten the landing page:
+      # * receive payment success notification
+      # * enroll customer into auth database
+
+      # on `checkout.session.expired`:
+      # * invalidate the nonce associated with the checkout session
+
+      # return 204 no content
+    end
+  end
 end
